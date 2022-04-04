ba3ccaa5bd30fd4eaa3a1a4834e1b58213c04442
Sami Mäkelä <mrsmkl@gmail.com>
2022-04-04 14:31:44 +0300

    selecting ops

--- src/vm.rs
+++ src/vm.rs
@@ -14,6 +14,8 @@ use ark_sponge::poseidon::PoseidonParameters;
 use ark_relations::r1cs::ConstraintSystem;
 use ark_r1cs_std::boolean::Boolean;
 use ark_r1cs_std::boolean::AllocatedBool;
+use ark_r1cs_std::select::CondSelectGadget;
+use ark_r1cs_std::fields::FieldVar;
 
 use crate::hash::{Params,poseidon,poseidon_gadget};
 
@@ -27,7 +29,7 @@ pub struct VM {
     pub frame_pointer: usize,
     pub mem_counter: usize,
     // pub pc: Vec<CodeTree>,
-    pub pc: Vec<u32>,
+    pub pc: Vec<u64>,
 }
 
 pub struct VMVar {
@@ -38,7 +40,7 @@ pub struct VMVar {
     pub pc_hash: FpVar<Fr>,
 }
 
-fn hash_code(params: &Params, code: &[u32]) -> Fr {
+fn hash_code(params: &Params, code: &[u64]) -> Fr {
     let mut res = Fr::from(0);
     for op in code.iter().rev() {
         // println!("hashing {:?}", op);
@@ -50,7 +52,7 @@ fn hash_code(params: &Params, code: &[u32]) -> Fr {
     res
 }
 
-fn decode(a: u32, n: usize) -> Vec<bool> {
+fn decode(a: u64, n: usize) -> Vec<bool> {
     let mut res = vec![];
     let mut a = a;
     for i in 0..n {
@@ -61,8 +63,37 @@ fn decode(a: u32, n: usize) -> Vec<bool> {
     res
 }
 
-fn decode_var(cs: &ConstraintSystemRef<Fr>, a: u32, n: usize) -> Vec<Boolean<Fr>> {
+fn decode_var(cs: &ConstraintSystemRef<Fr>, a: u64, n: usize) -> Vec<Boolean<Fr>> {
     decode(a, n).iter().map(|b| Boolean::from(AllocatedBool::<Fr>::new_witness(cs.clone(), || Ok(b)).unwrap())).collect::<Vec<_>>()
+}
+
+fn decode2(a: u128, n: usize) -> Vec<bool> {
+    let mut res = vec![];
+    let mut a = a;
+    for i in 0..n {
+        let x = a%2;
+        res.push(if x == 0 { false } else { true });
+        a = a/2;
+    }
+    res
+}
+
+fn decode_var2(cs: &ConstraintSystemRef<Fr>, a: u128, n: usize) -> Vec<Boolean<Fr>> {
+    decode2(a, n).iter().map(|b| Boolean::from(AllocatedBool::<Fr>::new_witness(cs.clone(), || Ok(b)).unwrap())).collect::<Vec<_>>()
+}
+
+fn decode_op(a: u64) -> (u64, usize, usize) {
+    (a%16, ((a/16)%4) as usize, ((a/(16*4))%4) as usize)
+}
+
+fn compute(op: u64, a: u64, b: u64) -> u128 {
+    if op == 0 {
+        (a as u128) + (b as u128)
+    } else if op == 1 {
+        (a as u128) * (b as u128)
+    } else /* if op == 2 */ {
+        (1u128 << 64) + (a as u128) - (b as u128)
+    }
 }
 
 fn generate_step(cs: &ConstraintSystemRef<Fr>, before: VM, before_var: VMVar) -> Result<VMVar, SynthesisError> {
@@ -73,160 +104,33 @@ fn generate_step(cs: &ConstraintSystemRef<Fr>, before: VM, before_var: VMVar) ->
     let next_pc_hash = hash_code(params, &before.pc[1..]);
     let next_hash = FpVar::Var(AllocatedFp::<Fr>::new_witness(cs.clone(), || Ok(Fr::from(next_pc_hash))).unwrap());
     // Instruction comes from boolean wires...
-
-    // let inst_var = FpVar::Var(AllocatedFp::<Fr>::new_witness(cs.clone(), || Ok(Fr::from(inst))).unwrap());
     let inst_var = Boolean::le_bits_to_fp_var(&inst_bools).unwrap();
     let prev_hash = poseidon_gadget(params, vec![inst_var, next_hash]);
     before_var.pc_hash.enforce_equal(&prev_hash);
 
+    // Select registers
+    let a_var = FpVar::conditionally_select_power_of_two_vector(&inst_bools[0..2], &before_var.registers).unwrap();
+    let b_var = FpVar::conditionally_select_power_of_two_vector(&inst_bools[2..4], &before_var.registers).unwrap();
 
+    let add_var = a_var.clone() + b_var.clone();
+    let mul_var = a_var.clone() * b_var.clone();
+    let minus_var = (a_var.clone() - b_var.clone()) + FpVar::constant(Fr::from(1u128 << 64));
+    let neg_var = FpVar::constant(Fr::from(1u128 << 64)) - a_var.clone();
 
+    // result will have 64 bits
+    // or for multiplication there will be 128 bits
+    let (op, r1, r2) = decode_op(inst);
+    let result = compute(op, before.registers[r1], before.registers[r2]);
+    let result_bools = decode_var2(cs, result, 128);
+    let result_var = Boolean::le_bits_to_fp_var(&result_bools[0..64]).unwrap();
+    let over_var = Boolean::le_bits_to_fp_var(&result_bools[64..128]).unwrap();
+    let combined_var = result_var.clone() + FpVar::constant(Fr::from(1u128 << 64)) * over_var.clone();
+
+    let ops = vec![add_var, mul_var, minus_var, neg_var];
+    let selected = FpVar::conditionally_select_power_of_two_vector(&inst_bools[4..6], &ops).unwrap();
+
+    // Then need to select the result registers
 
     Ok(before_var)
 }
 
-
-/*
-
-use crate::{VM,Transition,hash_list,hash_code};
-use crate::InstructionCircuit;
-
-#[derive(Debug, Clone)]
-pub struct AddManyCircuit {
-    pub steps: Vec<(VM,VM)>,
-    pub params: PoseidonParameters<Fr>,
-}
-
-impl InstructionCircuit for AddManyCircuit {
-    fn calc_hash(&self) -> Fr {
-        panic!("calc hash");
-    }
-    fn transition(&self) -> Transition {
-        panic!("transition")
-    }
-}
-
-fn generate_step(cs: ConstraintSystemRef<Fr>, params: PoseidonParameters<Fr>, params_g: &CRHParametersVar::<Fr>, before: VM, after: VM) -> Result<FpVar<Fr>, SynthesisError> {
-    let elen = before.expr_stack.len();
-
-    let pc_hash = hash_code(&params, &after.pc);
-    let stack_hash = hash_list(&params, &before.expr_stack[..elen-2].iter().map(|a| Fr::from(*a)).collect::<Vec<Fr>>());
-    let locals_hash = before.hash_locals(&params);
-    let control_hash = before.hash_control(&params);
-
-    let p1 = before.expr_stack[elen - 1];
-    let p2 = before.expr_stack[elen - 2];
-
-    let var_a = FpVar::Var(
-        AllocatedFp::<Fr>::new_witness(cs.clone(), || Ok(Fr::from(p1))).unwrap(),
-    );
-    let var_b = FpVar::Var(
-        AllocatedFp::<Fr>::new_witness(cs.clone(), || Ok(Fr::from(p2))).unwrap(),
-    );
-
-    let locals_var = FpVar::Var(
-        AllocatedFp::<Fr>::new_witness(cs.clone(), || Ok(locals_hash)).unwrap(),
-    );
-    let control_var = FpVar::Var(
-        AllocatedFp::<Fr>::new_witness(cs.clone(), || Ok(control_hash)).unwrap(),
-    );
-
-    let hash_pc_after_var = FpVar::Var(
-        AllocatedFp::<Fr>::new_witness(cs.clone(), || Ok(pc_hash)).unwrap(),
-    );
-
-    let mut inputs_pc = Vec::new();
-    inputs_pc.push(FpVar::Constant(Fr::from(1)));
-    inputs_pc.push(hash_pc_after_var.clone());
-    let hash_pc_gadget = CRHGadget::<Fr>::evaluate(&params_g, &inputs_pc).unwrap();
-
-    let mut inputs_stack_before2 = Vec::new();
-    inputs_stack_before2.push(var_b.clone());
-    inputs_stack_before2.push(FpVar::Var(
-        AllocatedFp::<Fr>::new_witness(cs.clone(), || Ok(stack_hash)).unwrap(),
-    ));
-    let hash_stack_before2_gadget = CRHGadget::<Fr>::evaluate(&params_g, &inputs_stack_before2).unwrap();
-
-    let mut inputs_stack_before = Vec::new();
-    inputs_stack_before.push(var_a.clone());
-    inputs_stack_before.push(hash_stack_before2_gadget);
-    let hash_stack_before_gadget = CRHGadget::<Fr>::evaluate(&params_g, &inputs_stack_before).unwrap();
-
-    let mut inputs_stack_after = Vec::new();
-    // TODO: check that 32-bit
-    inputs_stack_after.push(var_a.clone() + var_b.clone());
-    inputs_stack_after.push(FpVar::Var(
-        AllocatedFp::<Fr>::new_witness(cs.clone(), || Ok(stack_hash)).unwrap(),
-    ));
-    let hash_stack_after_gadget = CRHGadget::<Fr>::evaluate(&params_g, &inputs_stack_after).unwrap();
-
-    // Compute VM hash before
-    let mut inputs_vm_before = Vec::new();
-    inputs_vm_before.push(hash_pc_gadget);
-    inputs_vm_before.push(hash_stack_before_gadget);
-    inputs_vm_before.push(locals_var.clone());
-    inputs_vm_before.push(control_var.clone());
-    let hash_vm_before_gadget = CRHGadget::<Fr>::evaluate(&params_g, &inputs_vm_before).unwrap();
-
-    // Compute VM hash after
-    let mut inputs_vm_after = Vec::new();
-    inputs_vm_after.push(hash_pc_after_var);
-    inputs_vm_after.push(hash_stack_after_gadget);
-    inputs_vm_after.push(locals_var.clone());
-    inputs_vm_after.push(control_var.clone());
-    let hash_vm_after_gadget = CRHGadget::<Fr>::evaluate(&params_g, &inputs_vm_after).unwrap();
-
-    let mut inputs_transition = Vec::new();
-    inputs_transition.push(hash_vm_before_gadget.clone());
-    inputs_transition.push(hash_vm_after_gadget.clone());
-    let hash_transition_gadget = CRHGadget::<Fr>::evaluate(&params_g, &inputs_transition).unwrap();
-
-    Ok(hash_transition_gadget)
-}
-
-impl ConstraintSynthesizer<Fr> for AddManyCircuit {
-    fn generate_constraints(
-        self,
-        cs: ConstraintSystemRef<Fr>,
-    ) -> Result<(), SynthesisError> {
-        let params_g = CRHParametersVar::<Fr>::new_witness(cs.clone(), || Ok(self.params.clone())).unwrap();
-        // make each step
-        let mut vars = vec![];
-        for el in self.steps {
-            let var = generate_step(cs.clone(), self.params.clone(), &params_g, el.0, el.1);
-            vars.push(var);
-        }
-        // println!("num constraints {}, valid {}", cs.num_constraints(), cs.is_satisfied().unwrap());
-        println!("num constraints {}", cs.num_constraints());
-        Ok(())
-    }
-}
-
-pub fn test(params: &PoseidonParameters<Fr>, step: (VM, VM)) {
-    use ark_std::test_rng;
-    use std::time::Instant;
-    use crate::InnerSNARK;
-    use ark_crypto_primitives::CircuitSpecificSetupSNARK;
-    use ark_crypto_primitives::SNARK;
-    let cs_sys = ConstraintSystem::<Fr>::new();
-    let cs = ConstraintSystemRef::new(cs_sys);
-    let mut steps = vec![];
-    for i in 0..256 {
-        steps.push(step.clone())
-    }
-    let circuit = AddManyCircuit {
-        params: params.clone(),
-        steps,
-    };
-    // circuit.generate_constraints(cs);
-    let mut rng = test_rng();
-    println!("Setting up circuit");
-    let (pk, vk) = InnerSNARK::setup(circuit.clone(), &mut rng).unwrap();
-    println!("Testing prove");
-    let start = Instant::now();
-    let proof = InnerSNARK::prove(&pk, circuit.clone(), &mut rng).unwrap();
-    let elapsed = start.elapsed();
-    println!("proving took {} ms", elapsed.as_millis());
-}
-
-*/
