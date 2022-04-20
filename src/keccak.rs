512d0e63de86f67ac8e7b49df6de1592761aee1e
Sami Mäkelä <mrsmkl@gmail.com>
2022-04-20 14:05:07 +0300

    theta

--- src/keccak.rs
+++ src/keccak.rs
@@ -22,7 +22,7 @@ use ark_r1cs_std::boolean::Boolean;
 use crate::{VM,Transition,hash_list,hash_code};
 use crate::InstructionCircuit;
 
-fn shr_bool(a: Vec<Boolean<Fr>>, r: usize) -> Vec<Boolean<Fr>> {
+fn shr_bool(a: &Vec<Boolean<Fr>>, r: usize) -> Vec<Boolean<Fr>> {
     let mut out = vec![];
     for i in 0..a.len() {
         out.push(if i+r >= 64 { Boolean::FALSE } else { a[i-r].clone() })
@@ -30,7 +30,7 @@ fn shr_bool(a: Vec<Boolean<Fr>>, r: usize) -> Vec<Boolean<Fr>> {
     out
 }
 
-fn shl_bool(a: Vec<Boolean<Fr>>, r: usize) -> Vec<Boolean<Fr>> {
+fn shl_bool(a: &Vec<Boolean<Fr>>, r: usize) -> Vec<Boolean<Fr>> {
     let mut out = vec![];
     for i in 0..a.len() {
         out.push(if i < r { Boolean::FALSE } else { a[i-r].clone() })
@@ -46,7 +46,7 @@ fn or_bool(a: Vec<Boolean<Fr>>, b: Vec<Boolean<Fr>>) -> Vec<Boolean<Fr>> {
     out
 }
 
-fn xor_bool(a: Vec<Boolean<Fr>>, b: Vec<Boolean<Fr>>) -> Vec<Boolean<Fr>> {
+fn xor_bool(a: &[Boolean<Fr>], b: &[Boolean<Fr>]) -> Vec<Boolean<Fr>> {
     let mut out = vec![];
     for i in 0..a.len() {
         out.push(a[i].clone().xor(&b[i]).unwrap())
@@ -54,14 +54,79 @@ fn xor_bool(a: Vec<Boolean<Fr>>, b: Vec<Boolean<Fr>>) -> Vec<Boolean<Fr>> {
     out
 }
 
-fn perm_d(a: Vec<Boolean<Fr>>, b: Vec<Boolean<Fr>>, shl: usize, shr: usize) -> Vec<Boolean<Fr>> {
+fn xor5_bool(a: &[Boolean<Fr>], b: &[Boolean<Fr>], c: &[Boolean<Fr>], d: &[Boolean<Fr>], e: &[Boolean<Fr>]) -> Vec<Boolean<Fr>> {
+    let mut out = vec![];
+    for i in 0..a.len() {
+        out.push(a[i].clone().xor(&b[i]).unwrap().xor(&c[i]).unwrap().xor(&d[i]).unwrap().xor(&e[i]).unwrap())
+    }
+    out
+}
+
+fn perm_d(a: &Vec<Boolean<Fr>>, b: &Vec<Boolean<Fr>>, shl: usize, shr: usize) -> Vec<Boolean<Fr>> {
     // d = b ^ (a<<shl | a>>shr)
 
-    let aux0 = shr_bool(a.clone(), shr);
+    let aux0 = shr_bool(a, shr);
     let aux1 = shl_bool(a, shl);
 
     let aux2 = or_bool(aux0, aux1);
-    xor_bool(b, aux2)
+    xor_bool(&b, &aux2)
 }
 
+fn perm_theta(inp: Vec<Boolean<Fr>>) -> Vec<Boolean<Fr>> {
+    let c0 = xor5_bool(&inp[0..64], &inp[5*64..6*64], &inp[10*64..11*64], &inp[15*64..16*64], &inp[20*64..21*64]);
+    let c1 = xor5_bool(&inp[64..2*64], &inp[6*64..7*64], &inp[11*64..12*64], &inp[16*64..17*64], &inp[21*64..22*64]);
+    let c2 = xor5_bool(&inp[2*64..3*64], &inp[7*64..8*64], &inp[12*64..13*64], &inp[17*64..18*64], &inp[22*64..23*64]);
+    let c3 = xor5_bool(&inp[3*64..4*64], &inp[8*64..9*64], &inp[13*64..14*64], &inp[18*64..19*64], &inp[23*64..24*64]);
+    let c4 = xor5_bool(&inp[4*64..5*64], &inp[9*64..10*64], &inp[14*64..15*64], &inp[19*64..20*64], &inp[24*64..25*64]);
+
+    let mut r = vec![vec![]; 25];
+
+    // d = c4 ^ (c1<<1 | c1>>(64-1))
+    let d0 = perm_d(&c1, &c4, 1, 64-1);
+
+    // r[0] = a[0] ^ d
+    r[0] = xor_bool(&inp[0..64], &d0);
+    r[5] = xor_bool(&inp[5*64..6*64], &d0);
+    r[10] = xor_bool(&inp[10*64..11*64], &d0);
+    r[15] = xor_bool(&inp[15*64..16*64], &d0);
+    r[20] = xor_bool(&inp[20*64..21*64], &d0);
+
+    // d = c0 ^ (c2<<1 | c2>>(64-1))
+    let d1 = perm_d(&c2, &c0, 1, 64-1);
+    r[1] = xor_bool(&inp[1*64..2*64], &d1);
+    r[6] = xor_bool(&inp[6*64..7*64], &d1);
+    r[11] = xor_bool(&inp[11*64..12*64], &d1);
+    r[16] = xor_bool(&inp[16*64..17*64], &d1);
+    r[21] = xor_bool(&inp[21*64..22*64], &d1);
+
+    let d2 = perm_d(&c3, &c1, 1, 64-1);
+    r[2] = xor_bool(&inp[2*64..3*64], &d2);
+    r[7] = xor_bool(&inp[7*64..8*64], &d2);
+    r[12] = xor_bool(&inp[12*64..13*64], &d2);
+    r[17] = xor_bool(&inp[17*64..18*64], &d2);
+    r[22] = xor_bool(&inp[22*64..23*64], &d2);
+
+    let d3 = perm_d(&c4, &c2, 1, 64-1);
+    r[3] = xor_bool(&inp[3*64..4*64], &d3);
+    r[8] = xor_bool(&inp[8*64..9*64], &d3);
+    r[13] = xor_bool(&inp[13*64..14*64], &d3);
+    r[18] = xor_bool(&inp[18*64..19*64], &d3);
+    r[23] = xor_bool(&inp[23*64..24*64], &d3);
+
+    let d4 = perm_d(&c0, &c3, 1, 64-1);
+    r[4] = xor_bool(&inp[4*64..5*64], &d4);
+    r[9] = xor_bool(&inp[9*64..10*64], &d4);
+    r[14] = xor_bool(&inp[14*64..15*64], &d4);
+    r[19] = xor_bool(&inp[19*64..20*64], &d4);
+    r[24] = xor_bool(&inp[24*64..25*64], &d4);
+
+    let mut out = vec![];
+
+    for i in 0..25 {
+        for j in 0..64 {
+            out.push(r[i][j].clone())
+        }
+    }
+    out
+}
 
