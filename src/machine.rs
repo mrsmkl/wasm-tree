8342d5846365a8cbcbfb1423f9acca99ec731b0e
Sami Mäkelä <mrsmkl@gmail.com>
2022-05-06 16:27:27 +0300

    constant instr

--- src/machine.rs
+++ src/machine.rs
@@ -12,6 +12,7 @@ use ark_relations::r1cs::ConstraintSystemRef;
 use ark_r1cs_std::eq::EqGadget;
 use ark_sponge::poseidon::PoseidonParameters;
 use ark_r1cs_std::boolean::{AllocatedBool,Boolean};
+use ark_r1cs_std::fields::FieldVar;
 
 use crate::{VM,Transition,hash_code};
 use crate::InstructionCircuit;
@@ -77,6 +78,21 @@ fn hash_instruction(params: &Params, inst: &Instruction) -> FpVar<Fr> {
     ])
 }
 
+#[derive(Debug, Clone)]
+pub struct Value {
+    value: FpVar<Fr>,
+    ty: FpVar<Fr>,
+}
+
+fn hash_value(params: &Params, inst: &Value) -> FpVar<Fr> {
+    poseidon_gadget(&params, vec![
+        inst.value.clone(),
+        inst.ty.clone(),
+    ])
+}
+
+
+
 pub fn prove_instr(
     cs: ConstraintSystemRef<Fr>,
     params : &Params,
@@ -101,16 +117,20 @@ pub fn prove_instr(
     inst_var
 }
 
-// stack: perhaps there should just be several alternatives for different length stacks ...
-pub fn check_stack(
-    cs: ConstraintSystemRef<Fr>,
-    params : &Params,
-    vars: Vec<FpVar<Fr>>,
+#[derive(Debug, Clone)]
+pub struct Stack {
+    values: Vec<FpVar<Fr>>,
     base: FpVar<Fr>,
+}
+
+// stack: perhaps there should just be several alternatives for different length stacks ...
+pub fn hash_stack(
+    params : &Params,
+    stack: &Stack
 ) -> FpVar<Fr> {
     // compute root from base
-    let mut root = base.clone();
-    for el in vars.iter().rev() {
+    let mut root = stack.base.clone();
+    for el in stack.values.iter().rev() {
         root = poseidon_gadget(&params, vec![
             el.clone(),
             root.clone(),
@@ -118,4 +138,53 @@ pub fn check_stack(
     }
     root
 }
+
+impl Stack {
+    fn push(&mut self, v: FpVar<Fr>) {
+        self.values.push(v.clone());
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct MachineWithStack {
+    valueStack : Stack,
+    internalStack : Stack,
+    blockStack : Stack,
+    frameStack : Stack,
+
+    globalStateHash : FpVar<Fr>,
+    moduleIdx : FpVar<Fr>,
+    functionIdx : FpVar<Fr>,
+    functionPc : FpVar<Fr>,
+    modulesRoot : FpVar<Fr>,
+}
+
+// There can be savings by sharing the hashing of stacks ...
+pub fn elim_stack(params : &Params, mach: &MachineWithStack) -> Machine {
+    Machine {
+        valueStack : hash_stack(params, &mach.valueStack),
+        internalStack : hash_stack(params, &mach.internalStack),
+        blockStack : hash_stack(params, &mach.blockStack),
+        frameStack : hash_stack(params, &mach.frameStack),
+    
+        globalStateHash : mach.globalStateHash.clone(),
+        moduleIdx : mach.moduleIdx.clone(),
+        functionIdx : mach.functionIdx.clone(),
+        functionPc : mach.functionPc.clone(),
+        modulesRoot : mach.modulesRoot.clone(),
+    }
+}
+
+pub fn execute_const(params: &Params, mach: &MachineWithStack, expected: u32, ty: u32, inst: &Instruction) -> MachineWithStack {
+    let expected = FpVar::constant(Fr::from(expected));
+    inst.opcode.enforce_equal(&expected);
+    let mut mach = mach.clone();
+    let v = Value {
+        value: inst.argumentData.clone(),
+        ty: FpVar::constant(Fr::from(ty)),
+    };
+    mach.valueStack.push(hash_value(params, &v));
+    mach
+}
+
 
